cb9b2ca58af2ce789bcce290029cac79617215ce
Hasan Rauf <HasanR@winmo.com>
2023-12-21 18:20:12 -0600

    Adds the booking price engine

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,11 @@
 class Booking < ApplicationRecord
+  before_save :calculate_price
+
+  private
+
+  def calculate_price
+    base_charge = 20
+    additional_charge = animal_type == 'Dog' ? 10 : 5
+    self.price = base_charge + (additional_charge * hours_requested)
+  end
 end
