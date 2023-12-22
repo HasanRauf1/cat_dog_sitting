eca44b859269158870f91033e3731e6cb7359338
Hasan Rauf <HasanR@winmo.com>
2023-12-22 12:39:00 -0600

    remove comments

--- spec/features/admin_page_spec.rb
+++ spec/features/admin_page_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "AdminPage", type: :feature do
   scenario "User views all bookings" do
-    # Create a sample booking
     Booking.create!(first_name: "Jane", last_name: "Smith", animal_name: "Max", animal_type: "Cat", hours_requested: 3, date_of_service: Date.today)
 
     visit bookings_path
